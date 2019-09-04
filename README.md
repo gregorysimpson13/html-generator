d36ae2b0a7e80659fb25efc8c81a68abed32cf30
Gregory Simpson <gregorysimpson13@gmail.com>
2019-09-04 15:56:13 -0700

    updating readme and using argparser

---
+++ README.md
@@ -0,0 +1,7 @@
+#Example
+
+This example command should produce two files "page1.html" and "page2.html"
+
+```bash
+python main.py -t test.in.html -j details.json -o ./
+```
--- main.py
+++ main.py
@@ -3,11 +3,8 @@ https://google.github.io/styleguide/pyguide.html'''
 
 import json
 import os
-
-# GLOBALS
-IN_TEMPLATE = 'test.in.html'
-IN_JSON = 'details.json'
-OUTPUT_FOLDER = 'out/'
+import argparse
+from copy import deepcopy
 
 
 # read the file and return the contents
@@ -25,7 +22,7 @@ def get_json_contents(filename):
 
 
 # save the file
-def save_file_contents(filename, contents, output_folder=OUTPUT_FOLDER):
+def save_file_contents(filename, contents, output_folder):
     if output_folder[-1] != os.path.sep:
         output_folder += os.path.sep
     filepath = "%s%s" % (output_folder, filename)
@@ -35,18 +32,25 @@ def save_file_contents(filename, contents, output_folder=OUTPUT_FOLDER):
 
 
 # run the program
-def run_program():
+def run_program(template, data, output_folder):
     # gather all the resources
-    contents = get_file_contents('test.in.html')
-    data = get_json_contents(IN_JSON)
+    contents = get_file_contents(template)
+    data = get_json_contents(data)
     # loop through the json
     for file_details in data['files']:
+        new_contents = deepcopy(contents)
         new_filename = file_details.pop('filename')
         for key, details_var in file_details.items():
             key_subtitute = "${%s}" % (key)
-            new_contents = contents.replace(key_subtitute, details_var)
-        save_file_contents(new_filename, new_contents)
+            new_contents = new_contents.replace(key_subtitute, details_var)
+        save_file_contents(new_filename, new_contents, output_folder)
 
 
 if __name__ == "__main__":
-    run_program()
+    parser = argparse.ArgumentParser(
+        description='Create HTML files from template using json data')
+    parser.add_argument('-o', '--outdir', dest='output_folder', default='out')
+    parser.add_argument('-t', '--template', dest='template', required=True)
+    parser.add_argument('-j', '--json', dest='data', required=True)
+    args = parser.parse_args()
+    run_program(args.template, args.data, args.output_folder)
